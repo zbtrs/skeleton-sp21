1373b71827652e7c8130977bfe0130c930d006b9
zbtrs <1339623011@qq.com>
2022-08-04 00:11:10 -0700

    Timetest

--- lab3/timingtest/TimeAList.java
+++ lab3/timingtest/TimeAList.java
@@ -23,5 +23,21 @@ public class TimeAList {
 
     public static void timeAListConstruction() {
         // TODO: YOUR CODE HERE
+        AList<Integer> Ns = new AList<>();
+        AList<Double> times = new AList<>();
+        AList<Integer> temp = new AList<>();
+        Stopwatch sw = new Stopwatch();
+        int[] goals = new int[]{1000,2000,4000,8000,16000,32000,64000,128000};
+        int cur = 0;
+        for (int i = 0; i < goals.length; i++) {
+            while (cur < goals[i]) {
+                cur++;
+                temp.addLast(cur);
+            }
+            double timeInSeconds = sw.elapsedTime();
+            Ns.addLast(goals[i]);
+            times.addLast(timeInSeconds);
+        }
+        printTimingTable(Ns,times,Ns);
     }
 }
